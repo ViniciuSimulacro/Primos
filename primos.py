56cc461f4d95bb3e67a66f5dd663a1610b0bdf0d
ViniciusSimulacro <delucca86@gmail.com>
2022-07-30 18:51:31 -0300

    Código p/ descobrir Nº primo

---
+++ primos.py
@@ -0,0 +1,23 @@
+def primos():
+    numero = int(input('Digite um numero para saber se é primo: '))
+    divisor = 1
+    cont = 1
+   
+    while cont < numero+1:
+        resultado = 0
+        resultado = numero % divisor
+        if resultado == 0:
+            cont += 1
+        
+        if numero == 1 or cont > 2:
+            print(f'O numero {numero} não é primo')
+            break
+                
+        if numero == divisor+1 and cont == 2:
+            print(f'O numero {numero} é primo ')
+            break
+
+        divisor += 1
+        
+
+primos()
